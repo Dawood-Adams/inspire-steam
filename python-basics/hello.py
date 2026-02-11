2378fae176637616db75fb2f6285af3fd9c83923
Dawood-Adams <adamsdawood363@gmail.com>
2026-02-11 09:17:48 +0300

    first commit

---
+++ python-basics/hello.py
@@ -0,0 +1,15 @@
+# This is a comment
+print("Hello from Dawood")
+# my city
+print("I am from Kisumu")
+# my hobbies
+print("I like to play football,learning computer reading novels and cooking")
+age = 17
+height = 1.80
+weight = 78
+print(f"I am {age} years old")
+print(f"I am {height} meters tall")
+print(f"I weigh {weight} kilograms")
+hobby = "playing_football" 
+print(f"I like {hobby}")
+
