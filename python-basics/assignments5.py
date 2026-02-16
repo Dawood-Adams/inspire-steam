fad57248e027e3e718d605db553f99fa4ad01876
Dawood-Adams <adamsdawood363@gmail.com>
2026-02-16 10:27:11 +0300

    How to use the if/else clause

---
+++ python-basics/assignments5.py
@@ -0,0 +1,20 @@
+# Name : Dawood Adams
+# Date : 12/02/2026
+# Program to calculate the income tax
+
+salary = int(input("Enter your gross salary:"))
+
+if salary <= 50000:
+    tax = (0.025 * salary)
+    net_salary = salary - tax
+    print(f"The gross salary is:", {salary})
+    print(f"The net salary is:", {net_salary})
+    print(f"The tax is:", {tax})
+
+elif salary for range(50000,100000)
+    tax = (0.045 * salary)
+    net_salary = salary - tax
+    print(f"The gross salary is:", {salary})
+    print(f"The net salary is:", {net_salary})
+    print(f"The tax is:", {tax})
+
