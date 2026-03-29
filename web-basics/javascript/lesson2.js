e2987575220ed62d40e1992484609fd91fa5c0df
Dawood-Adams <adamsdawood363@gmail.com>
2026-03-29 15:45:49 +0300

    My first official website

---
+++ web-basics/javascript/lesson2.js
@@ -0,0 +1,11 @@
+//This is a singleline comment
+//*This is a multiline comment
+
+age = 28
+
+if (age>=18){
+    console.log("You are allowed to drive")
+}
+else{
+    console.log("You are not allowed to drive")
+}
